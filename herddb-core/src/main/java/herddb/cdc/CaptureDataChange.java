629b074068f31401693d211c76456a18635d895a
Enrico Olivelli <eolivelli@gmail.com>
2021-07-16 17:34:42 +0200

    More CDC implemetation work

--- herddb-core/src/main/java/herddb/cdc/CaptureDataChange.java
+++ herddb-core/src/main/java/herddb/cdc/CaptureDataChange.java
@@ -19,16 +19,22 @@
  */
 package herddb.cdc;
 
+import java.util.HashMap;
 import java.util.Map;
 
 import herddb.client.ClientConfiguration;
 import herddb.cluster.BookkeeperCommitLog;
 import herddb.cluster.BookkeeperCommitLogManager;
 import herddb.cluster.ZookeeperMetadataStorageManager;
+import herddb.codec.DataAccessorForFullRecord;
+import herddb.codec.RecordSerializer;
 import herddb.log.CommitLog;
 import herddb.log.LogEntry;
+import herddb.log.LogEntryType;
 import herddb.log.LogSequenceNumber;
 import herddb.metadata.MetadataStorageManagerException;
+import herddb.model.Record;
+import herddb.model.Table;
 import herddb.server.ServerConfiguration;
 import org.apache.bookkeeper.stats.NullStatsLogger;
 
@@ -43,16 +49,54 @@ public class CaptureDataChange implements AutoCloseable {
     public enum MutationType {
         INSERT,
         UPDATE,
-        DELETE
+        DELETE,
+        CREATETABLE,
+        DELETETABLE,
+        ALTERTABLE
     }
 
-    public interface Mutation {
-        String getTableNane();
-        String getTableSpaceName();
-        MutationType getMutationType();
-        Map<String, Object> getPrimaryKey();
-        Map<String, Object> getValue();
-        LogSequenceNumber getLogSequenceNumber();
+    public static class Mutation {
+        private final Table table;
+        private final MutationType mutationType;
+        private final DataAccessorForFullRecord record;
+        private final LogSequenceNumber logSequenceNumber;
+        private final long timestamp;
+
+        public Mutation(Table table, MutationType mutationType,
+                DataAccessorForFullRecord record, LogSequenceNumber logSequenceNumber,
+                long timestamp)
+        {
+            this.table = table;
+            this.mutationType = mutationType;
+            this.record = record;
+            this.logSequenceNumber = logSequenceNumber;
+            this.timestamp = timestamp;
+        }
+
+        public Table getTable()
+        {
+            return table;
+        }
+
+        public MutationType getMutationType()
+        {
+            return mutationType;
+        }
+
+        public DataAccessorForFullRecord getRecord()
+        {
+            return record;
+        }
+
+        public LogSequenceNumber getLogSequenceNumber()
+        {
+            return logSequenceNumber;
+        }
+
+        public long getTimestamp()
+        {
+            return timestamp;
+        }
     }
 
     public interface MutationListener {
@@ -73,6 +117,8 @@ public class CaptureDataChange implements AutoCloseable {
         this.tableSpaceUUID = tableSpaceUUID;
     }
 
+    private Map<String, Table> tablesDefinition = new HashMap<>();
+
     public void run() throws Exception {
         try (ZookeeperMetadataStorageManager zookeeperMetadataStorageManager = buildMetadataStorageManager(configuration);
              BookkeeperCommitLogManager manager = new BookkeeperCommitLogManager(zookeeperMetadataStorageManager, new ServerConfiguration(), NullStatsLogger.INSTANCE);)
@@ -86,6 +132,7 @@ public class CaptureDataChange implements AutoCloseable {
                     @Override
                     public boolean accept(LogSequenceNumber lsn, LogEntry entry) throws Exception
                     {
+                        applyEntry(entry, lsn);
                         return !closed;
                     }
                 }, context);
@@ -96,15 +143,70 @@ public class CaptureDataChange implements AutoCloseable {
         closed = true;
     }
 
-    private static ZookeeperMetadataStorageManager buildMetadataStorageManager(ClientConfiguration configuration) throws MetadataStorageManagerException
+    private void applyEntry(LogEntry entry, LogSequenceNumber lsn) throws Exception
+    {
+        switch (entry.type)
+        {
+            case LogEntryType.NOOP:
+            case LogEntryType.CREATE_INDEX:
+            case LogEntryType.DROP_INDEX:
+                break;
+            case LogEntryType.DROP_TABLE:
+            {
+                Table table = tablesDefinition.remove(entry.tableName);
+                listener.accept(new Mutation(table, MutationType.DELETETABLE, null, lsn, entry.timestamp));
+            }
+            ;
+            break;
+            case LogEntryType.CREATE_TABLE:
+            {
+                Table table = Table.deserialize(entry.value.to_array());
+                tablesDefinition.put(entry.tableName, table);
+                listener.accept(new Mutation(table, MutationType.CREATETABLE, null, lsn, entry.timestamp));
+            }
+            ;
+            break;
+            case LogEntryType.INSERT:
+            {
+                Table table = tablesDefinition.get(entry.tableName);
+                DataAccessorForFullRecord record = new DataAccessorForFullRecord(table, new Record(entry.key, entry.value));
+                listener.accept(new Mutation(table, MutationType.CREATETABLE, record, lsn, entry.timestamp));
+            }
+            ;
+            break;
+            case LogEntryType.DELETE:
+            {
+                Table table = tablesDefinition.get(entry.tableName);
+                DataAccessorForFullRecord record = new DataAccessorForFullRecord(table, new Record(entry.key, entry.value));
+                listener.accept(new Mutation(table, MutationType.DELETE, record, lsn, entry.timestamp));
+            }
+            ;
+            break;
+            case LogEntryType.UPDATE:
+            {
+                Table table = tablesDefinition.get(entry.tableName);
+                DataAccessorForFullRecord record = new DataAccessorForFullRecord(table, new Record(entry.key, entry.value));
+                listener.accept(new Mutation(table, MutationType.UPDATE, record, lsn, entry.timestamp));
+            }
+            ;
+            break;
+            case LogEntryType.BEGINTRANSACTION:
+            case LogEntryType.COMMITTRANSACTION:
+            case LogEntryType.ROLLBACKTRANSACTION:
+                break;
+            default:
+                // discard unkwown entry types
+                break;
+        }
+    }
+    private static ZookeeperMetadataStorageManager buildMetadataStorageManager(ClientConfiguration configuration)
+        throws MetadataStorageManagerException
     {
         String zkAddress = configuration.getString(PROPERTY_ZOOKEEPER_ADDRESS, PROPERTY_ZOOKEEPER_ADDRESS_DEFAULT);
         String zkPath = configuration.getString(PROPERTY_ZOOKEEPER_PATH, PROPERTY_ZOOKEEPER_PATH_DEFAULT);
         int sessionTimeout = configuration.getInt(PROPERTY_ZOOKEEPER_SESSIONTIMEOUT, 60000);
         ZookeeperMetadataStorageManager zk = new ZookeeperMetadataStorageManager(zkAddress, sessionTimeout, zkPath);
-        zk.start(false /*
-         * formatIfNeeded
-         */);
+        zk.start(false);
         return zk;
     }
 }
