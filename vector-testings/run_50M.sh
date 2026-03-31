af998995d4f1d34e1fd19cbbec792b9e06b88656
Enrico Olivelli <eolivelli@gmail.com>
2026-03-31 16:54:16 +0200

    VectorBench: change defaults

--- vector-testings/run_50M.sh
+++ vector-testings/run_50M.sh
@@ -16,4 +16,4 @@
 # under the License.
 #
 set -x
-./run.sh --password hdb --batch-size 30000 --ingest-threads 24 --dataset sift10m -n 50000000 --checkpoint --drop-table "@0"
+./run.sh --password hdb --batch-size 5000 --ingest-threads 8 --dataset sift10m -n 50000000 --checkpoint --index-before-ingest --drop-table "@0"
